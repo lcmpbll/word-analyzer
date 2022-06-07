7fdec524f79e79b6bc6d28b6f9cb18bfd1ba2203
Liam Campbell <liam22campbell@gmail.com>
2022-06-07 14:24:28 -0700

    Counts the number of words that are the same.
    Co-Authored-By Peter Vang<vang_peter06@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,45 @@
 
 
 function wordCounter(text) {
-	if (text.length === 0) {
-		return 0;
-	}
-	let wordCount = 0;
-	const wordArray = text.split(" ");
-	wordArray.forEach(function(word){
-		wordCount++;
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+		if (!Number(element)){
+			wordCount++;
+		}
 	});
-	return wordCount;
+  return wordCount;
 }
 
-wordCounter(" ");
-console.log(wordCount);
+wordCounter("            ");
+
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+
+// //*function numberOfOccurrencesInText(word, text) {
+// 	const wordArray = text.split(" ");
+// 	let wordCount = 0;
+// 	wordArray.forEach(function(element) {
+// 		if (word === element) {
+// 		wordCount++;
+//     }
+//   });
+// 		return wordCount;
+// }
+// //
+
