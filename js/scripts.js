0f971f0490f8bcbcaeac2e06db526db2f7feaef5
Liam Campbell <liam22campbell@gmail.com>
2022-06-07 21:26:37 -0700

    BUGFIX, empty single word count value no longer returns the value of total words.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 // Business Logic
-
-
-
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -16,10 +13,8 @@ function wordCounter(text) {
   return wordCount;
 }
 
-wordCounter("            ");
-
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -32,16 +27,18 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+//UI Logic
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
 
-// //*function numberOfOccurrencesInText(word, text) {
-// 	const wordArray = text.split(" ");
-// 	let wordCount = 0;
-// 	wordArray.forEach(function(element) {
-// 		if (word === element) {
-// 		wordCount++;
-//     }
-//   });
-// 		return wordCount;
-// }
-// //
+
 
