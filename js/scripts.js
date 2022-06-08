206cba09d13fbb6ce32a35688d86ef194258456f
Liam Campbell <liam22campbell@gmail.com>
2022-06-07 21:59:34 -0700

    Add bolding to user selected words in returned paragraph.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+//Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length ===0) || (word.trim().length === 0));
+}
+
 // Business Logic
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -14,7 +20,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -27,6 +33,26 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+function boldPassage(word, text) {
+  if (noInputtedWord(word,text)){
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !==(textArray.length -1)){
+    htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>"
+}
+
+
 //UI Logic
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
@@ -37,6 +63,7 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
 
